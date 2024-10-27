4b41c8e809e8d56773875b2f6f94177aa072e01b
zHd4 <computersystembase@gmail.com>
2024-10-27 17:35:49 +0100

    Create key-gen.py

---
+++ key-gen.py
@@ -0,0 +1,24 @@
+import argparse
+
+import pgpy
+from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, CompressionAlgorithm, SymmetricKeyAlgorithm
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="PGP keys generation tool")
+
+    parser.add_argument("name", type=str, help="Your name")
+    parser.add_argument("email", type=str, help="Email")
+    parser.add_argument("key_size", type=str, help="Key size")
+
+    args = parser.parse_args()
+
+    key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 2048)
+    uid = pgpy.PGPUID.new("User Name", email="user@example.com")
+    key.add_uid(uid, usage={KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
+                hashes=[HashAlgorithm.SHA256], ciphers=[SymmetricKeyAlgorithm.AES256],
+                compression=[CompressionAlgorithm.ZLIB])
+
+    with open("private_key.asc", "w") as f:
+        f.write(str(key))
+    with open("public_key.asc", "w") as f:
+        f.write(str(key.pubkey))
