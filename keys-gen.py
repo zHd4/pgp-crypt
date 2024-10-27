dd19c5ebbd387fdd773a4495f98011db322ae883
zHd4 <computersystembase@gmail.com>
2024-10-27 18:54:02 +0100

    Update keys-gen.py

--- keys-gen.py
+++ keys-gen.py
@@ -1,36 +1,49 @@
 import argparse
 import os
 
-import pgpy
-from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, CompressionAlgorithm, SymmetricKeyAlgorithm
+import gnupg
+
+def get_password() -> str or None:
+    user_password = input('Password: ')
+    password_confirmation = input('Confirm password: ')
+
+    if user_password == password_confirmation:
+        return user_password
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PGP keys generation tool')
 
-    parser.add_argument('name', type=str, help='Your name')
     parser.add_argument('email', type=str, help='Email')
     parser.add_argument('key_size', type=int, help='Key size')
 
     args = parser.parse_args()
+    gpg = gnupg.GPG()
+
+    password = get_password()
+
+    if password is None:
+        print('Passwords don`t match')
+        exit()
 
     print('Generating...')
 
-    key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, args.key_size)
-    uid = pgpy.PGPUID.new(args.name, email=args.email)
+    input_data = gpg.gen_key_input(
+        name_email=args.email,
+        passphrase=password
+    )
 
-    key.add_uid(uid, usage={KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
-                hashes=[HashAlgorithm.SHA256], ciphers=[SymmetricKeyAlgorithm.AES256],
-                compression=[CompressionAlgorithm.ZLIB])
+    key = gpg.gen_key(input_data)
 
     output_dir_path = f'{args.name} <{args.email}>'
 
     if not os.path.isdir(output_dir_path):
         os.mkdir(output_dir_path)
 
-    with open(f'{output_dir_path}/private.asc', 'w') as f:
-        f.write(str(key))
-
     with open(f'{output_dir_path}/public.asc', 'w') as f:
-        f.write(str(key.pubkey))
+        f.write(gpg.export_keys(key.fingerprint))
+
+    with open(f'{output_dir_path}/private.asc', 'w') as f:
+        f.write(gpg.export_keys(key.fingerprint, True, passphrase="secure_passphrase"))
 
     print(f'Saved to: "{os.path.abspath(output_dir_path)}"')
