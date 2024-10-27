f26b5a68e850e6413885d6e03939d2e4287ed5c2
zHd4 <computersystembase@gmail.com>
2024-10-27 17:41:09 +0100

    Update key-gen.py

--- key-gen.py
+++ key-gen.py
@@ -1,14 +1,15 @@
 import argparse
+import os
 
 import pgpy
 from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, CompressionAlgorithm, SymmetricKeyAlgorithm
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="PGP keys generation tool")
+    parser = argparse.ArgumentParser(description='PGP keys generation tool')
 
-    parser.add_argument("name", type=str, help="Your name")
-    parser.add_argument("email", type=str, help="Email")
-    parser.add_argument("key_size", type=int, help="Key size")
+    parser.add_argument('name', type=str, help='Your name')
+    parser.add_argument('email', type=str, help='Email')
+    parser.add_argument('key_size', type=int, help='Key size')
 
     args = parser.parse_args()
 
@@ -19,7 +20,13 @@ if __name__ == '__main__':
                 hashes=[HashAlgorithm.SHA256], ciphers=[SymmetricKeyAlgorithm.AES256],
                 compression=[CompressionAlgorithm.ZLIB])
 
-    with open("private_key.asc", "w") as f:
+    output_dir_path = f'{args.name} <{args.email}>'
+
+    if not os.path.isdir(output_dir_path):
+        os.mkdir(output_dir_path)
+
+    with open(f'{output_dir_path}/private.asc', 'w') as f:
         f.write(str(key))
-    with open("public_key.asc", "w") as f:
+
+    with open(f'{output_dir_path}/public.asc', 'w') as f:
         f.write(str(key.pubkey))
