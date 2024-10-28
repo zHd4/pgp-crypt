0e865085e87095b97421a448eb3fd2e2ee58f7c5
zHd4 <computersystembase@gmail.com>
2024-10-28 09:30:26 +0100

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('public_key_path', type=str, help='Path to public key')
     parser.add_argument('input_file_path', type=str, help='Path to file to be encrypted')
     parser.add_argument('-o', '--output-path', type=str, help='Output path')
+    parser.add_argument('-a', '--armor', action='store_true', help='Text output')
 
     args = parser.parse_args()
     gpg = gnupg.GPG()
@@ -22,10 +23,7 @@ if __name__ == '__main__':
         gpg.import_keys(file.read())
 
     with open(args.input_file_path, 'rb') as file:
-        status = gpg.encrypt_file(
-            file, recipients=[args.email],
-            output=output_path
-        )
+        status = gpg.encrypt_file(file, recipients=[args.email], output=output_path, armor=args.armor)
 
     if status.ok:
         print(f'File encrypted! Saved to: {output_path}')
