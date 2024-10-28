0ff988e3f36509a51499cb6db15158e876fe8549
zHd4 <computersystembase@gmail.com>
2024-10-28 09:44:31 +0100

    Refactor keys-gen.py

--- keys-gen.py
+++ keys-gen.py
@@ -3,6 +3,7 @@ import os
 
 import gnupg
 
+
 def get_password() -> str or None:
     user_password = input('Password: ')
     password_confirmation = input('Confirm password: ')
@@ -11,35 +12,34 @@ def get_password() -> str or None:
         return user_password
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='PGP keys generation tool')
+parser = argparse.ArgumentParser(description='PGP keys generation tool')
 
-    parser.add_argument('email', type=str, help='Email')
-    parser.add_argument('key_size', type=int, help='Key size')
+parser.add_argument('email', type=str, help='Email')
+parser.add_argument('key_size', type=int, help='Key size')
 
-    args = parser.parse_args()
-    gpg = gnupg.GPG()
+args = parser.parse_args()
+gpg = gnupg.GPG()
 
-    password = get_password()
+password = get_password()
 
-    if password is None:
-        print("Passwords don't match")
-        exit()
+if password is None:
+    print("Passwords don't match")
+    exit()
 
-    print('Generating...')
+print('Generating...')
 
-    input_data = gpg.gen_key_input(name_email=args.email, passphrase=password)
-    key = gpg.gen_key(input_data)
+input_data = gpg.gen_key_input(name_email=args.email, passphrase=password)
+key = gpg.gen_key(input_data)
 
-    output_dir_path = f'{args.email}'
+output_dir_path = f'{args.email}'
 
-    if not os.path.isdir(output_dir_path):
-        os.mkdir(output_dir_path)
+if not os.path.isdir(output_dir_path):
+    os.mkdir(output_dir_path)
 
-    with open(f'{output_dir_path}/public.asc', 'w') as f:
-        f.write(gpg.export_keys(key.fingerprint))
+with open(f'{output_dir_path}/public.asc', 'w') as f:
+    f.write(gpg.export_keys(key.fingerprint))
 
-    with open(f'{output_dir_path}/private.asc', 'w') as f:
-        f.write(gpg.export_keys(key.fingerprint, True, passphrase=password))
+with open(f'{output_dir_path}/private.asc', 'w') as f:
+    f.write(gpg.export_keys(key.fingerprint, True, passphrase=password))
 
-    print(f'Saved to: "{os.path.abspath(output_dir_path)}"')
+print(f'Saved to: "{os.path.abspath(output_dir_path)}"')
