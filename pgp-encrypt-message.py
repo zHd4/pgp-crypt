6a9de3f79830940463cf3c11db5d6aef9f439486
zHd4 <computersystembase@gmail.com>
2024-11-03 14:00:19 +0100

    Update pgp-encrypt-message.py

--- pgp-encrypt-message.py
+++ pgp-encrypt-message.py
@@ -4,8 +4,8 @@ import gnupg
 
 parser = argparse.ArgumentParser(description='PGP messages encryption tool')
 
-parser.add_argument('-e', '--email', type=str, help="Recipient's email")
-parser.add_argument('-k', '--key', type=str, help='Path to public key')
+parser.add_argument('email', type=str, help="Recipient's email")
+parser.add_argument('key', type=str, help='Path to public key')
 
 args = parser.parse_args()
 gpg = gnupg.GPG()
@@ -25,17 +25,8 @@ def multiline_input(message: str) -> str:
 
     return '\n'.join(contents)
 
-
-if args.email is None:
-    email = input("Recipient's email: ")
-else:
-    email = args.email
-
-if args.key is None:
-    key = multiline_input('Paste your PGP public key here:\n')
-else:
-    with open(args.key, 'r') as file:
-        key = file.read()
+with open(args.key, 'r') as file:
+    key = file.read()
 
 gpg.import_keys(key)
 
@@ -44,7 +35,7 @@ message_text = multiline_input('\nEnter your message '
                                'Ctrl+C to cancel, Enter to finish):\n')
 
 print()
-result = gpg.encrypt(message_text, [email])
+result = gpg.encrypt(message_text, [args.key])
 
 if result.ok:
     print(result.data.decode('utf-8'))
